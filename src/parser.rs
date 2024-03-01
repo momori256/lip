897efc40e206e7f0929a26d41dc61c24d1b6a4c3
momori-nakano <momori.nakano@gmail.com>
2024-02-29 16:27:26 -0800

    Add tokenize and parse for if expression

--- src/parser.rs
+++ src/parser.rs
@@ -31,10 +31,28 @@ impl std::fmt::Display for Operator {
 }
 
 #[derive(Debug, PartialEq, Eq)]
+pub struct If {
+    pub cond: Box<Expr>,
+    pub then: Box<Expr>,
+    pub other: Box<Expr>,
+}
+
+impl If {
+    pub fn new(cond: Expr, then: Expr, other: Expr) -> Self {
+        Self {
+            cond: Box::new(cond),
+            then: Box::new(then),
+            other: Box::new(other),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
 pub enum Expr {
     Bool(bool),
     Operator(Operator),
     Call(Operator, Vec<Expr>),
+    If(If),
 }
 
 pub fn parse(tokens: &[Token]) -> Result<Expr, ParserErr> {
@@ -56,6 +74,9 @@ fn parse_internal(tokens: &[Token]) -> Result<(Expr, usize), ParserErr> {
             Token::Not => Ok((Expr::Operator(Operator::Not), 1)),
             _ => Err(ParserErr::Parse(format!("invalid token `{first:?}`"))),
         };
+    }
+    if tokens[1] == Token::If {
+        return parse_if(tokens);
     }
     parse_call(tokens)
 }
@@ -96,6 +117,32 @@ fn parse_call(tokens: &[Token]) -> Result<(Expr, usize), ParserErr> {
     Ok((Expr::Call(operator, operands), p + 1))
 }
 
+fn parse_if(tokens: &[Token]) -> Result<(Expr, usize), ParserErr> {
+    let len = tokens.len();
+    if len < 6 {
+        return Err(ParserErr::Parse("if expression is too short".to_string()));
+    }
+    if tokens[0] != Token::Lparen || tokens[1] != Token::If {
+        return Err(ParserErr::Parse(format!(
+            "if expression must start with `( if`, not `{:?} {:?}`",
+            tokens[0], tokens[1]
+        )));
+    }
+    let mut p = 2;
+    let (cond, cnt) = parse_internal(&tokens[p..])?;
+    p += cnt;
+    let (then, cnt) = parse_internal(&tokens[p..])?;
+    p += cnt;
+    let (other, cnt) = parse_internal(&tokens[p..])?;
+    p += cnt;
+    if tokens[p] != Token::Rparen {
+        return Err(ParserErr::Parse(
+            "if expression is not closed with `)`".to_string(),
+        ));
+    }
+    Ok((Expr::If(If::new(cond, then, other)), p + 1))
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -111,6 +158,10 @@ mod tests {
 
     fn not(exprs: Vec<Expr>) -> Expr {
         Expr::Call(Operator::Not, exprs)
+    }
+
+    fn if_expr(cond: Expr, then: Expr, other: Expr) -> Expr {
+        Expr::If(If::new(cond, then, other))
     }
 
     #[test]
@@ -148,6 +199,22 @@ mod tests {
                 or(vec![Expr::Bool(false), Expr::Bool(false), Expr::Bool(true)]),
                 not(vec![Expr::Bool(true)])
             ]),
+            expr
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn parse_if_succeed() -> Result<(), Box<dyn std::error::Error>> {
+        let tokens = tokenizer::tokenize("(if (& T T) T (| F F))")?;
+        let (expr, cnt) = parse_internal(&tokens)?;
+        assert_eq!(tokens.len(), cnt);
+        assert_eq!(
+            if_expr(
+                and(vec![Expr::Bool(true), Expr::Bool(true)]),
+                Expr::Bool(true),
+                or(vec![Expr::Bool(false), Expr::Bool(false)])
+            ),
             expr
         );
         Ok(())
