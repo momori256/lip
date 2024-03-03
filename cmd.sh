ac9e973ed5a457d0bd89586a23bd01b8f3ccfc17
momori-nakano <momori.nakano@gmail.com>
2024-03-02 16:43:24 -0800

    Clean WASM files

--- cmd.sh
+++ cmd.sh
@@ -2,9 +2,11 @@
 
 case "$1" in
 "build")
-	wasm-pack build --out-dir ./doc/pkg
+	wasm-pack build --no-pack --out-dir ./www/pkg
+	rm ./www/pkg/.gitignore ./www/pkg/README.md
 	;;
 "start")
-	npm --prefix doc run start
+	npm --prefix www install
+	npm --prefix www run start
 	;;
 esac
---
+++ www/pkg/lip.d.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export class Repl {
+  free(): void;
+/**
+* @returns {Repl}
+*/
+  static new(): Repl;
+/**
+* @param {string} expr
+* @returns {string}
+*/
+  eval(expr: string): string;
+}
