3e64f03f9318d69537bddf3930e1efa7271d5dc7
momori-nakano <momori.nakano@gmail.com>
2024-03-03 16:10:48 -0800

    Add WASM to lp

---
+++ lp/www/index.js
@@ -0,0 +1,31 @@
+import init, { Repl } from "./pkg/lp.js";
+
+(async () => {
+  await init();
+  exec();
+})();
+
+function exec() {
+  const input = document.querySelector("input");
+  const history = document.querySelector("ol");
+
+  document.querySelector("button").addEventListener("click", () => {
+    const text = input.value;
+    if (!text.length) {
+      return;
+    }
+    let output;
+    try {
+      output = Repl.eval(text);
+    } catch (e) {
+      output = e;
+    }
+    const element = () => {
+      const li = document.createElement("li");
+      li.innerHTML = `<pre>${text}</pre> => ${output}`;
+      return li;
+    };
+    history.appendChild(element());
+    input.value = "";
+  });
+}
